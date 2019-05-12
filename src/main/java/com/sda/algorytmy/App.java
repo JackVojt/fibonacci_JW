1abd15a10e6c7c837ba4470e8390399cecf2e8e3
Jacek.Woj <jacek.wojtuszek@gmail.com>
2019-05-12 11:09:14 +0100

    fibonacci_2

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,18 +1,37 @@
 package com.sda.algorytmy;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        int tab[] = new int [21];
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        fib2(x);
+        int tab[] = new int[21];
         tab[0] = 0;
         tab[1] = 1;
-        for (int i = 2; i < tab.length; i++){
-            tab[i] = tab[i-2] + tab[i-1];
+        for (int i = 2; i < tab.length; i++) {
+            tab[i] = tab[i - 2] + tab[i - 1];
             System.out.println(tab[i]);
         }
-
-
     }
 
+    public static void fib2(int x) {
+
+        if (x < 2) {
+            System.out.println(x);
+        }
+        int a = 0;
+        int b = 1;
+        int wynik = 1;
+        for (int i = 2; i < x; i++) {
+            a = b;
+            b = wynik;
+            wynik = a + b;
+        }
+        System.out.println(wynik);
+
+    }
 }
 
 

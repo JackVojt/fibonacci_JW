1923cedb4b9ba6b3d3ec12031edcf175fde52f13
Jacek.Woj <jacek.wojtuszek@gmail.com>
2019-05-12 10:26:42 +0100

    recursive solution

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,13 +1,17 @@
 package com.sda.algorytmy;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        int tab[] = new int [21];
+        tab[0] = 0;
+        tab[1] = 1;
+        for (int i = 2; i < tab.length; i++){
+            tab[i] = tab[i-2] + tab[i-1];
+            System.out.println(tab[i]);
+        }
+
     }
+
 }
+
+
--- src/main/java/com/sda/algorytmy/fib_reku.java
+++ src/main/java/com/sda/algorytmy/fib_reku.java
@@ -1,4 +1,17 @@
 package com.sda.algorytmy;
 
 public class fib_reku {
+    public static void main(String[] args) {
+        System.out.println(fib(20));
+    }
+
+    public static int fib(int n) {
+        if (n == 0)
+            return 0;
+        else if (n == 1)
+            return 1;
+        else
+            return fib(n - 2) + fib(n - 1);
+    }
 }
+
